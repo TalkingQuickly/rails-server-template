625428d493bfd1c06d3c712deb0551480c98dbd2
Ben Dixon <ben@hillsbede.co.uk>
2013-11-03 20:05:08 +0000

    adds environments to make compatible with chef 11.8.0

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -2,6 +2,7 @@ cookbook_path ["cookbooks", "site-cookbooks"]
 node_path     "nodes"
 role_path     "roles"
 data_bag_path "data_bags"
+environment_path "environments"
 #encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
