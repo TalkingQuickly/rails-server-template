5e95717da020f58d7f79480025c232c117556162
Ben Dixon <ben@hillsbede.co.uk>
2016-08-22 09:02:32 +0100

    better way to do knife.rb hooks

--- knife.rb
+++ knife.rb
@@ -1,7 +1,14 @@
 local_mode true
 chef_repo_path   File.expand_path('../' , __FILE__)
+cookbook_path ["cookbooks", "berks-cookbooks"]
 
 knife[:ssh_attribute] = "knife_zero.host"
 knife[:use_sudo] = true
-knife[:editor] = '/usr/local/bin/sublime'
-cookbook_path ["berks-cookbooks"]
+knife[:editor] = '/usr/local/bin/vim'
+knife[:before_bootstrap] = "berks vendor"
+knife[:before_converge]  = "berks vendor"
+
+# Automatically run berks vendor before converging
+# if ARGV[0..1] == ["zero", "converge"] && ! Chef::Config.has_key?(:color)
+#  system('berks vendor')
+# end
