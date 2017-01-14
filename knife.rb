d381df6103f5f83933507643aed0ed26be23619d
Ben Dixon <ben@hillsbede.co.uk>
2017-01-14 12:21:32 +0000

    updates: auto vendoring method

--- knife.rb
+++ knife.rb
@@ -7,8 +7,3 @@ knife[:use_sudo] = true
 knife[:editor] = '/usr/local/bin/vim'
 knife[:before_bootstrap] = "berks vendor"
 knife[:before_converge]  = "berks vendor"
-
-# Automatically run berks vendor before converging
-# if ARGV[0..1] == ["zero", "converge"] && ! Chef::Config.has_key?(:color)
-#  system('berks vendor')
-# end
